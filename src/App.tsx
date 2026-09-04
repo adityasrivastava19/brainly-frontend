509d91719b0ff67c65697fcdaf4615e63fa2a0db
Aditya Srivastava <aditya123@gmail.com>
2026-09-04 14:20:59 +0530

    card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { ShareIcon } from '../icon/shareicon';
-import { PlusIcon } from '../icon/plusicon';
 import './App.css';
 import { Button } from "./component/ui/Button";
 import { Card } from './component/ui/Card';
 function App() {
   return (
     <div className="flex gap-2 p-4 ">
-      <Card/>
+      <Card type="tweeter" link="#" title="Example card" />
     </div>
   );
 }
--- src/component/ui/Card.tsx
+++ src/component/ui/Card.tsx
@@ -1,26 +1,40 @@
 import { PlusIcon } from "../../../icon/plusicon";
 import { ShareIcon } from "../../../icon/shareicon";
-
-export function Card()
+import { DeleteIcon } from '../../../icon/deleteicon';
+import { YoutubeIcon } from '../../../icon/youtubeIcon';
+interface cardProps{
+    type:"youtube"|"tweeter",
+    link:string,
+    title:string 
+}
+export function Card({ type, link, title }: cardProps)
 {
     return( 
-    <div className="min-w-60 max-w-70 min-h-80 max-h-90 bg-white p-4 rounded-md  outline outline-slate-200 border border-gray-100">
+    <div className="min-w-60 max-w-70 min-h-80 max-h-90 bg-white p-4 rounded-md  outline outline-slate-200 border border-gray-100 overflow-hidden">
         <div className="flex justify-between ">
-            <div className="flex items-center">
-                <div>
-                <ShareIcon/>
+            <div className="flex items-center text-gray-800">
+                <div className="w-10 h-10 rounded-md cursor-pointer transition-shadow hover:shadow-md hover:shadow-gray-400 hover:bg-gray-200 flex items-center justify-center">
+                <YoutubeIcon/>
                 </div>
-                <div className="text-md">
-                 Project idea
+                <div className="text-lg">
+                {title}
                  </div>
             </div>
-            <div className="flex">
-                <ShareIcon/>
-                <PlusIcon/>
+            <div className="flex text-gray-800">
+                <div className="pr-2 w-10 h-10 rounded-md cursor-pointer transition-shadow hover:shadow-md hover:shadow-gray-400 hover:bg-gray-200 flex items-center justify-center">
+                    <ShareIcon/>
+                    </div>
+                <div className="w-10 h-10 rounded-md cursor-pointer transition-shadow hover:shadow-md hover:shadow-gray-400 hover:bg-gray-200 flex items-center justify-center">
+                    <DeleteIcon/>
+                    </div> 
             </div>
         </div>
-        <div className="w-full mt-8 rounded-md overflow-hidden">
-         <iframe className="w-full aspect-video block" src="https://www.youtube.com/embed/m8JHLzV2M3s?si=G38-da4na5CWAI13" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-    </div>
+        {type==="tweeter"&&  <div className=" flex justify-content origin-top scale-y-50 scale-x-90 mb-6 cursor-pointer">
+         <blockquote className="twitter-tweet"> <a href="https://x.com/ajeetbharti/status/2094532559724261666?ref_src=twsrc%5Etfw">August 31, 2026</a></blockquote> <script async src="https://platform.x.com/widgets.js" charSet="utf-8"></script>
+    </div> }
+    {type==="youtube"&& <div>
+        <iframe className="w-full mt-6 rounded-md" src="https://www.youtube.com/embed/YlbY94c0w8s?si=oBgHPbSLCmpkz2bR" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    </div>}
+     
     </div>)
 }
